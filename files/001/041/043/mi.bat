a95f93c594d7aa17f9e0c662e897a434d25b31ea
littleflute <13699175164@sina.cn>
2017-03-27 21:25:06 -0600

    Update mi.bat

--- files/001/041/043/mi.bat
+++ files/001/041/043/mi.bat
@@ -1,4 +1,4 @@
-; v0.0.3
+; v0.0.4
 del index.html
 
 echo ^<!DOCTYPE html^> >> index.html 
@@ -9,7 +9,7 @@ echo ^<button onclick="getCurTime()" type="button"^>Get current time position^</
 echo ^<button onclick="setCurTime()" type="button"^>Set time position to 5 seconds^</button^>^<br^> >>index.html 
 
 echo ^<video id="myVideo" width="320" height="176" controls^> >>index.html
-echo  ^<source src="mov_bbb.mp4" type="video/mp4"^>  >>index.html
+echo  ^<source src="dvd1/L1.mp4" type="video/mp4"^>  >>index.html
 
 echo Your browser does not support HTML5 video. >>index.html
 echo ^</video^>  >> index.html
