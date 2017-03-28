f0ed2c8c840d5276d686d8b8efd19ce1d5531ecd
littleflute <13699175164@sina.cn>
2017-03-27 21:33:24 -0600

    Update mi.bat

--- files/001/041/043/mi.bat
+++ files/001/041/043/mi.bat
@@ -1,4 +1,4 @@
-; v0.0.4
+; v0.0.5
 del index.html
 
 echo ^<!DOCTYPE html^> >> index.html 
@@ -6,10 +6,11 @@ echo ^<html^>  >> index.html
 echo ^<body^>  >> index.html
 
 echo ^<button onclick="getCurTime()" type="button"^>Get current time position^</button^> >>index.html
+echo ^<button onclick="plus50ms()" type="button"^>Time >> 50ms^</button^> >>index.html
 echo ^<button onclick="setCurTime()" type="button"^>Set time position to 5 seconds^</button^>^<br^> >>index.html 
 
 echo ^<video id="myVideo" width="320" height="176" controls^> >>index.html
-echo  ^<source src="dvd1/L1.mp4" type="video/mp4"^>  >>index.html
+echo  ^<source src="dvd1/L.mp4" type="video/mp4"^>  >>index.html
 
 echo Your browser does not support HTML5 video. >>index.html
 echo ^</video^>  >> index.html
@@ -21,11 +22,16 @@ echo function getCurTime() { >> index.html
 echo    alert(vid.currentTime);  >> index.html
 echo }  >> index.html 
 
+
 echo function setCurTime() { >> index.html
 echo    vid.currentTime=5;  >> index.html
 echo } >> index.html 
-echo ^</script^> >> index.html 
 
+
+echo function plus50ms() { >> index.html
+echo    vid.currentTime=5;  >> index.html
+echo } >> index.html 
+echo ^</script^> >> index.html
 
 echo ^</body^>  >> index.html 
 echo ^</html>  >> index.html
