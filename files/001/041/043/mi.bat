240806f0d5bf1abefde7e66debfa2de42c26709c
littleflute <13699175164@sina.cn>
2017-03-27 21:21:42 -0600

    Update mi.bat

--- files/001/041/043/mi.bat
+++ files/001/041/043/mi.bat
@@ -1,4 +1,4 @@
-; v0.0.1
+; v0.0.2
 del index.html
 
 echo ^<!DOCTYPE html^> >> index.html 
@@ -10,7 +10,7 @@ echo ^<button onclick="setCurTime()" type="button"^>Set time position to 5 secon
 
 echo ^<video id="myVideo" width="320" height="176" controls^> >>index.html
 echo  ^<source src="mov_bbb.mp4" type="video/mp4"^>  >>index.html
-  <source src="mov_bbb.ogg" type="video/ogg">
+
 echo Your browser does not support HTML5 video. >>index.html
 echo ^</video^>  >> index.html
 
@@ -26,7 +26,6 @@ echo    vid.currentTime=5;  >> index.html
 echo } >> index.html 
 echo ^</script^> >> index.html 
 
-<p>Video courtesy of <a href="https://www.bigbuckbunny.org/" target="_blank">Big Buck Bunny</a>.</p>
 
 echo ^</body^>  >> index.html 
 echo ^</html>  >> index.html
